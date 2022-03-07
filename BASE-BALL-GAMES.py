887e8b5f6d3a91ad16a678c790c846f0a8d8ff33
Apoorv Mangalam <53372470+A-K-M16@users.noreply.github.com>
2022-03-07 08:49:20 +0530

    Create BASE-BALL-GAMES.py

---
+++ BASE-BALL-GAMES.py
@@ -0,0 +1,30 @@
+import itertools
+
+def get_value(word, substitution):
+    s = 0
+    factor = 1
+    for letter in reversed(word):
+        s += factor * substitution[letter]
+        factor *= 10
+    return s
+
+def solve(equation):
+    left, right = equation.lower().replace(' ', '').split('=')
+    left = left.split('+')
+    letters = set(right)
+    for word in left:
+        for letter in word:
+            letters.add(letter)
+    letters = list(letters)
+
+    digits = range(10)
+    for perm in itertools.permutations(digits, len(letters)):
+        sol = dict(zip(letters, perm))
+
+        if sum(get_value(word, sol) for word in left) == get_value(right, sol):
+            if(sol[right[0]]==1):
+                print(' + '.join(str(get_value(word, sol)) for word in left) + " = {} \n\nMapping: {}".format(get_value(right, sol), sol))
+                break
+
+print('BASE + BALL = GAMES ')
+solve('BASE + BALL = GAMES ')
